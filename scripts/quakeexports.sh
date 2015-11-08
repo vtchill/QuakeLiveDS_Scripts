16933c2fc05bda2237d26f2f800cf41be029131e
Jesse <manning.jesse@gmail.com>
2015-11-08 01:56:20 -0700

    update server tags

--- scripts/quakeexports.sh
+++ scripts/quakeexports.sh
@@ -8,7 +8,7 @@ export QLDS_X86="$QLDS_DIR/run_server_x86.sh"
 export QLDS_X64="$QLDS_DIR/run_server_x64.sh"
 export QLDS_CONFIG_URL="https://raw.githubusercontent.com/vtchill/QuakeLiveDS_Scripts/master"
 export QLDS_CONFIG_DIR="$QLDS_USER_HOME/.quakelive/config/baseq3"
-export QLDS_TAGS="US,CO,epoch"
+export QLDS_TAGS="US,TX,epoch"
 
 # private
 # export QLDS_RCON_PW=XXXX

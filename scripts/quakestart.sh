eff5b4bcea0b236fbcf00587c2b26c5a0abbd234
Jesse <manning.jesse@gmail.com>
2015-11-08 00:03:06 -0700

    update start script options

--- scripts/quakestart.sh
+++ scripts/quakestart.sh
@@ -24,19 +24,19 @@ exec $QLDS_X86 \
   +set net_strict 1 \
   +set net_port $gameport \
   +set sv_hostname "EPOCH VQL Duel #$servernum" \
+  +set fs_homepath "$QLDS_CONFIG_DIR" \
+  +set sv_tags "$QLDS_TAGS" \
+  +set sv_mappoolFile "mappool_vqlduel.txt" \
+  +set bot_enable 1 \
+  +set bot_nochat 1 \
+  +set g_voteFlags "13320" \
+  +set g_allowSpecVote 0 \
+  +set g_allowVoteMidGame 0 \
+  +set g_damage_lg 6 \
+  +set g_accessFile "access.txt" \
   +set zmq_rcon_enable 1 \
   +set zmq_rcon_password "$QLDS_RCON_PW" \
   +set zmq_rcon_port $rconport \
   +set zmq_stats_enable 1 \
   +set zmq_stats_password "" \
-  +set zmq_stats_port $gameport \
-  +set sv_tags "$QLDS_LOCATION_TAGS" \
-  +set g_voteFlags "13320" \
-  +set g_allowSpecVote 0 \
-  +set g_allowVoteMidGame 0 \
-  +set bot_enable 1 \
-  +set bot_nochat 1 \
-  +set g_damage_lg 6 \
-  +set g_accessFile "access.txt" \
-  +set sv_mappoolFile "mappool_vqlduel.txt" \
-  +set fs_homepath "$QLDS_CONFIG_DIR"
+  +set zmq_stats_port $gameport

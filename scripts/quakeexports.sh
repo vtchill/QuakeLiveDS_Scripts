9a28a0ae2efc249ebdee315868c731c91c166f86
Jesse <manning.jesse@gmail.com>
2015-11-08 00:35:48 -0700

    update config dir to be ~/.quakelive/config/baseq3

--- scripts/quakeexports.sh
+++ scripts/quakeexports.sh
@@ -6,7 +6,7 @@ export QLDS_WORKSHOP_DIR="$QLDS_DIR/steamapps/workshop"
 export QLDS_X86="$QLDS_DIR/run_server_x86.sh"
 export QLDS_X64="$QLDS_DIR/run_server_x64.sh"
 export QLDS_CONFIG_URL="https://raw.githubusercontent.com/vtchill/QuakeLiveDS_Scripts/master"
-export QLDS_CONFIG_DIR="$HOME/.quakelive/config"
+export QLDS_CONFIG_DIR="$HOME/.quakelive/config/baseq3"
 export QLDS_TAGS="US,CO,epoch"
 
 # private
